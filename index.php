4fe512cc9f851f7bc5b1b1ff7099d35793aac4dc
Kulwinder Billen <kbillen92@gmail.com>
2015-03-18 19:31:16 -0400

    Added a font, and the config.ini file

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 <head>
 <meta charset="UTF-8">
 <title>Counters</title>
-</head>
+	
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 
+</head>
 <body>
+
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
 
 
 </body>
-
 </html>
